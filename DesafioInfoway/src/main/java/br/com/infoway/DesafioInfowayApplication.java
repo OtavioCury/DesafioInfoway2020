9b901f7814b7ef2f1cda1ebe0def85fc4fca5cf0
Otávio Cury <otaviocury.oc@gmail.com>
2020-03-04 20:57:58 -0300

    Configurações iniciais do projeto Spring Boot

---
+++ DesafioInfoway/src/main/java/br/com/infoway/DesafioInfowayApplication.java
@@ -0,0 +1,13 @@
+package br.com.infoway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioInfowayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioInfowayApplication.class, args);
+	}
+
+}
---
+++ DesafioInfoway/src/test/java/br/com/infoway/DesafioInfowayApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.infoway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioInfowayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
