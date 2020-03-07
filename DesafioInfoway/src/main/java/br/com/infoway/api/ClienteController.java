0a1ad0af45d9432b1748c88bac67bed05ebef1dc
Otávio Cury <otaviocury.oc@gmail.com>
2020-03-07 10:17:37 -0300

    Criando controller para o cadastro de cliente

--- DesafioInfoway/src/main/java/br/com/infoway/api/ClienteController.java
+++ DesafioInfoway/src/main/java/br/com/infoway/api/ClienteController.java
@@ -1,6 +1,7 @@
 package br.com.infoway.api;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.infoway.dto.ClienteDTO;
+import br.com.infoway.exception.MensagemException;
 import br.com.infoway.service.ClienteService;
 
 @RestController
@@ -18,8 +20,19 @@ public class ClienteController {
 	private ClienteService clienteService;
 	
 	@RequestMapping(method = RequestMethod.POST)
-	public ResponseEntity<?> salvarGestor(@RequestBody ClienteDTO cliente) throws Exception {
-		return ResponseEntity.ok(clienteService.inserir(cliente));
+	public ResponseEntity<Object> salvarCliente(@RequestBody ClienteDTO cliente){
+		try {
+			return ResponseEntity.ok(clienteService.inserir(cliente));
+		} catch (org.springframework.dao.DataIntegrityViolationException e) {
+			return ResponseEntity
+		            .status(HttpStatus.INTERNAL_SERVER_ERROR)
+		            .body(MensagemException.emailExistente);
+		} catch(javax.validation.ConstraintViolationException e) {
+			return ResponseEntity
+		            .status(HttpStatus.INTERNAL_SERVER_ERROR)
+		            .body(e.getMessage());
+		}
+		
 	}
 
 }
