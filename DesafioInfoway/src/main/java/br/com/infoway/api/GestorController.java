080c63c23769c68aa55a854f6fc84d3a7d98f92b
Otávio Cury <otaviocury.oc@gmail.com>
2020-03-07 10:17:19 -0300

    Criando controller para o cadastro de gestor

--- DesafioInfoway/src/main/java/br/com/infoway/api/GestorController.java
+++ DesafioInfoway/src/main/java/br/com/infoway/api/GestorController.java
@@ -1,6 +1,7 @@
 package br.com.infoway.api;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.com.infoway.dto.GestorDTO;
 import br.com.infoway.dto.LanchoneteDTO;
+import br.com.infoway.exception.MensagemException;
 import br.com.infoway.modelo.Lanchonete;
 import br.com.infoway.service.GestorService;
 import br.com.infoway.service.LanchoneteService;
@@ -16,17 +18,27 @@ import br.com.infoway.service.LanchoneteService;
 @RestController
 @RequestMapping(value="/gestor")
 public class GestorController {
-	
+
 	@Autowired
 	private GestorService gestorService;
 	@Autowired
 	private LanchoneteService lanchoneteService;
-	
+
 	@RequestMapping(method = RequestMethod.POST)
-	public ResponseEntity<?> salvarGestor(@RequestBody GestorDTO gestor) throws Exception {
-		LanchoneteDTO lanchoneteDTO = new LanchoneteDTO(gestor.getLanchonete());
-		Lanchonete lanchonete = lanchoneteService.inserir(lanchoneteDTO);
-		return ResponseEntity.ok(gestorService.inserir(gestor, lanchonete));
+	public ResponseEntity<Object> salvarGestor(@RequestBody GestorDTO gestor) throws Exception {
+		try {
+			LanchoneteDTO lanchoneteDTO = new LanchoneteDTO(gestor.getLanchonete());
+			Lanchonete lanchonete = lanchoneteService.inserir(lanchoneteDTO);
+			return ResponseEntity.ok(gestorService.inserir(gestor, lanchonete));
+		} catch (org.springframework.dao.DataIntegrityViolationException e) {
+			return ResponseEntity
+					.status(HttpStatus.INTERNAL_SERVER_ERROR)
+					.body(MensagemException.emailExistente);
+		} catch(javax.validation.ConstraintViolationException e) {
+			return ResponseEntity
+					.status(HttpStatus.INTERNAL_SERVER_ERROR)
+					.body(e.getMessage());
+		}
 	}
 
 }
