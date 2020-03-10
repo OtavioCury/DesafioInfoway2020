0c1952d019080cf652181d71e215ba49a5377001
Otávio Cury <otaviocury.oc@gmail.com>
2020-03-09 22:14:17 -0300

    Adicionando mensagens de exceção relacionadas a pedido

--- DesafioInfoway/src/main/java/br/com/infoway/exception/MensagemException.java
+++ DesafioInfoway/src/main/java/br/com/infoway/exception/MensagemException.java
@@ -4,4 +4,6 @@ public class MensagemException{
 	
 	static public String emailExistente = "Email indisponível! Tente outro";
 	static public String produtoEsgotado = "Produto esgotado!";
+	static public String pedidoIdentificacao = "Pedido com id incorreto!";
+	static public String pedidoStatusGestor = "Status inválido!";
 }
