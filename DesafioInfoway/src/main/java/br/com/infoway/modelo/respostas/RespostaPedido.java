c65ffe13ed0de0c8d971fa4a99a601d72601ff50
Otávio Cury <otaviocury.oc@gmail.com>
2020-03-11 15:59:14 -0300

    Adicionando JsonCreator na classe RespostaPedido

--- DesafioInfoway/src/main/java/br/com/infoway/modelo/respostas/RespostaPedido.java
+++ DesafioInfoway/src/main/java/br/com/infoway/modelo/respostas/RespostaPedido.java
@@ -3,6 +3,8 @@ package br.com.infoway.modelo.respostas;
 import java.io.Serializable;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+
 import br.com.infoway.modelo.Pedido;
 import br.com.infoway.modelo.Produto;
 
@@ -13,7 +15,7 @@ public class RespostaPedido implements Serializable{
 	private static final long serialVersionUID = 1L;
 	private Pedido pedido;
 	private List<Produto> produtos;
-	
+	@JsonCreator
 	public RespostaPedido(Pedido pedido, List<Produto> produtos) {
 		super();
 		this.pedido = pedido;
